1257e48cb4dba4ac2f93811c8e7f6cbb6705c4fe
Sowmya Duvvuri <dsssri17@gmail.com>
2020-07-08 16:58:48 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'meeting-organizer-frontend'`, () => {
+  it(`should have as title 'meeting-organizer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('meeting-organizer-frontend');
+    expect(app.title).toEqual('meeting-organizer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('meeting-organizer-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('meeting-organizer app is running!');
   });
 });
